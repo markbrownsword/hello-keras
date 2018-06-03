fb94521ef3ee7167a2461298a42b529147f42cec
Mark Brownsword <markbrownsword@gmail.com>
2018-06-03 10:28:19 +1000

    format results

--- hellokeras/binary_classifier.py
+++ hellokeras/binary_classifier.py
@@ -1,4 +1,4 @@
-from tensorflow.python.keras.preprocessing.text import Tokenizer
+from tensorflow.python.keras.preprocessing import text
 from tensorflow.python.keras import models
 from tensorflow.python.keras import layers
 
@@ -7,29 +7,31 @@ import numpy as np
 
 class BinaryClassifier:
 
-    def __init__(self, model: models.Sequential=None, tokenizer: Tokenizer=None):
-        if model is None:
-            model = models.Sequential()
+    def __init__(self):
+        self.model = models.Sequential()
+        self.tokenizer = text.Tokenizer()
 
-        if tokenizer is None:
-            tokenizer = Tokenizer()
-
-        self.model = model
-        self.tokenizer = tokenizer
-
-    def train_model(self, data: list):
+    def train(self, data: list):
         features, labels = self.__process_text(data, True)
         n_words = features.shape[1]
-
         self.model.add(layers.Dense(16, activation='relu', input_shape=(n_words,)))
         self.model.add(layers.Dense(1, activation='sigmoid'))
         self.model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
         self.model.fit(features, labels, epochs=10, verbose=2)
 
-    def evaluate_model(self, data: list):
+    def evaluate(self, data: list):
         features, labels = self.__process_text(data)
-        _, acc = self.model.evaluate(features, labels, verbose=0)
-        return acc * 100
+        _, accuracy = self.model.evaluate(features, labels, verbose=0)
+        return accuracy
+
+    def predict(self, input_text: str):
+        raw_features = [input_text]
+        features = self.__vectorize_features(raw_features)
+        predictions = self.model.predict(features)
+        result = predictions[0, 0]
+        if round(result) == 0:
+            return result, 'NEGATIVE'
+        return result, 'POSITIVE'
 
     def __process_text(self, items: list, fit_tokenizer: bool=False):
         raw_features = []
@@ -47,9 +49,12 @@ class BinaryClassifier:
             self.tokenizer.fit_on_texts(raw_features)
 
         # Vectorise Features
-        features = self.tokenizer.texts_to_matrix(raw_features, mode='binary')  # one hot results
+        features = self.__vectorize_features(raw_features)
 
         # Vectorise Labels
         labels = np.asarray(raw_labels).astype('float32')
 
         return features, labels
+
+    def __vectorize_features(self, features: list):
+        return self.tokenizer.texts_to_matrix(features, mode='binary')  # one hot results
